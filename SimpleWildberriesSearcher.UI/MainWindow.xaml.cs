01f54d05f6de92e4d160cee2d1e97037178f1c04
Dzianis Shastak <shastak.dzianis@gmail.com>
2022-08-04 23:02:02 +0300

    adding event handlers for UI controls
    + adding extra styles to TextBoxes

--- SimpleWildberriesSearcher.UI/MainWindow.xaml.cs
+++ SimpleWildberriesSearcher.UI/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -27,6 +28,9 @@ namespace SimpleWildberriesSearcher.UI
         }
 
         #region Assistants
+        /// <summary>
+        /// Sets window icon. Just that simple.
+        /// </summary>
         private void SetIcon()
         {
             string iconPath = string.Concat(AppDomain.CurrentDomain.BaseDirectory, "Resources\\wb_logo.ico");
@@ -38,16 +42,28 @@ namespace SimpleWildberriesSearcher.UI
         #region Event Handlers
         private void BtnOpenCategoriesFile_Click(object sender, RoutedEventArgs e)
         {
-
+            using (OpenFileDialog dialog = new() { Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*" })
+            {
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    this.TxtCategoriesFile.Text = dialog.FileName;
+            }
         }
+
         private void BtnOpenOutputFolder_Click(object sender, RoutedEventArgs e)
         {
-
+            using (FolderBrowserDialog dialog = new())
+            {
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    this.TxtOutputFolder.Text = dialog.SelectedPath + System.IO.Path.DirectorySeparatorChar;
+            }
         }
+
         private void InputTextBoxes_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            bool areTextBoxesFilled = this.TxtCategoriesFile.Text.Trim().Length > 0 && this.TxtOutputFolder.Text.Trim().Length > 0;
+            this.BtnExport.IsEnabled = areTextBoxesFilled;
         }
+
         private void BtnExport_Click(object sender, RoutedEventArgs e)
         {
 
