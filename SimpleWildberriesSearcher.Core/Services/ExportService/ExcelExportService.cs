5fb611362faae95916bd3fbfc7b37c617cc6e909
Dzianis Shastak <shastak.dzianis@gmail.com>
2022-08-06 00:38:42 +0300

    adding Export service

---
+++ SimpleWildberriesSearcher.Core/Services/ExportService/ExcelExportService.cs
@@ -0,0 +1,33 @@
+﻿using OfficeOpenXml;
+using SimpleWildberriesSearcher.Core.Models;
+
+namespace SimpleWildberriesSearcher.Core.Services.ExportService
+{
+    /// <summary>
+    /// Service which is used to export collections of cards to an Excel file.
+    /// </summary>
+    public class ExcelExportService : IExportService
+    {
+        /// <inheritdoc />
+        /// <param name="filePath">Path of an Excel file where collections should be exported to.</param>
+        public async Task ExportAsync(string filePath, IEnumerable<ICardCollection> items)
+        {
+            FileInfo fileInfo = new FileInfo(filePath);
+
+            if (File.Exists(filePath))
+                File.Delete(filePath);
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            using (ExcelPackage package = new(fileInfo))
+            {
+                foreach (var collection in items)
+                {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets.Add(collection.Name);
+                    worksheet.Cells.LoadFromCollection(collection.Cards, PrintHeaders: true);
+                }
+
+                await package.SaveAsync();
+            }
+        }
+    }
+}
---
+++ SimpleWildberriesSearcher.Core/Services/ExportService/IExportService.cs
@@ -0,0 +1,16 @@
+﻿using SimpleWildberriesSearcher.Core.Models;namespace SimpleWildberriesSearcher.Core.Services.ExportService
+{
+    /// <summary>
+    /// Provides a contract to export collections of cards to different sources.
+    /// </summary>
+    public interface IExportService
+    {
+        /// <summary>
+        /// Asynchronously exports collections of cards using provided connection.
+        /// </summary>
+        /// <param name="connection">Destination of a source to export to.</param>
+        /// <param name="items">Collections of cards to export.</param>
+        /// <returns></returns>
+        Task ExportAsync(string connection, IEnumerable<ICardCollection> items);
+    }
+}
